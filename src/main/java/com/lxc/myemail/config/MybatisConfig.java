e993b0481a1b76eeb9458125e7caa9600c7c77e9
hyacinth-prog <1693019995A@gmail.com>
2022-01-05 18:25:53 +0800

    test 1

--- src/main/java/com/lxc/myemail/config/MybatisConfig.java
+++ src/main/java/com/lxc/myemail/config/MybatisConfig.java
@@ -7,4 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan(basePackages = "com.lxc.myemail.mapper")
 public class MybatisConfig {
 
+
 }
--- src/main/java/com/lxc/myemail/controller/BaseController.java
+++ src/main/java/com/lxc/myemail/controller/BaseController.java
@@ -9,13 +9,5 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 
 public class BaseController {
-//    protected static JsonFactory factory = mapper.getJsonFactory();
-//    /**将对象转成json输出**/
-//    @Autowired
-//    HttpResponse httpResponse;
-//    protected void writeJSON(Object obj) throws IOException {
-//        httpResponse.setContentType("text/html;charset=utf-8");
-//        JsonGenerator responseJsonGenerator = factory.createJsonGenerator(response.getOutputStream(), JsonEncoding.UTF8);
-//        responseJsonGenerator.writeObject(obj);
-//    }
+
 }
